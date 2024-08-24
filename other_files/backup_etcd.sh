6733149269100dd31c14827ffd8f26be57b98e05
rmiravalles <miravalles@gmail.com>
2024-08-24 15:10:14 +0200

    an update

--- other_files/backup_etcd.sh
+++ other_files/backup_etcd.sh
@@ -8,8 +8,7 @@ sudo apt install -y etcd-client
 export ETCDCTL_API=3
 
 # Perform a snapshot backup of the current state of etcd
-etcdctl snapshot save \
+etcdctl snapshot save etcd-backup \
     --cacert /etc/kubernetes/pki/etcd/ca.crt \
     --cert /etc/kubernetes/pki/etcd/server.crt \
     --key /etc/kubernetes/pki/etcd/server.key \
-    /tmp/etcd-backup.db
